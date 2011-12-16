630d78d6692af2475d7ce6a6f26cb7c23053feb5
oleksandr dodatko <dodikk88.reg@gmail.com>
2011-12-16 13:20:44 +0200

    Removed obsolete comments from the script

--- scripts/RelativePath.py
+++ scripts/RelativePath.py
@@ -1,10 +1,6 @@
 import sys; 
 import os.path; 
 
-## headerName = sys.argv[3];
-## frameworkDir = sys.argv[4];
-## frameworkHeadersDir = frameworkDir + '/Headers';
-
 headerPathFromFind = sys.argv[2];
 srcRoot = sys.argv[1];
 
